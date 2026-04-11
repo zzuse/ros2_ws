02d2d4edf7676eaee7ff1a198f9fa478116931ba
zzuse <zzuse@outlook.com>
2026-04-11 05:51:47 +0000

    client cpp update parameter

--- src/demo_cpp_service/src/patrol_client.cpp
+++ src/demo_cpp_service/src/patrol_client.cpp
@@ -1,9 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "chapt4_interfaces/srv/patrol.hpp"
 #include <chrono>
+#include "rcl_interfaces/msg/parameter.h"
+#include "rcl_interfaces/msg/parameter_value.hpp"
+#include "rcl_interfaces/msg/parameter_type.hpp"
+#include "rcl_interfaces/srv/set_parameters.hpp"
+
 using Patrol = chapt4_interfaces::srv::Patrol;
 using namespace std::chrono_literals;
-
+using SetP = rcl_interfaces::srv::SetParameters;
 
 class PatrolClientNode : public rclcpp::Node
 {
@@ -36,6 +41,52 @@ public:
              // 等待结果
         });
     }
+
+    SetP::Response::SharedPtr call_set_parameter(const rcl_interfaces::msg::Parameter &param)
+    {
+        auto param_client = this->create_client<SetP>("/turtle_control_node/set_parameters");
+        while (!param_client->wait_for_service(1s)) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+                return nullptr;
+            }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service not available, waiting again...");
+        }
+
+        auto request = std::make_shared<SetP::Request>();
+        request->parameters.push_back(param);
+
+        auto result_future = param_client->async_send_request(request);
+        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) == rclcpp::FutureReturnCode::SUCCESS) {
+            return result_future.get();
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Failed to call service set_parameters");
+            return nullptr;
+        }
+    }
+
+    void update_server_param_k(double k) {
+        rcl_interfaces::msg::Parameter param;
+        param.name = "k";
+        param.value.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+        param.value.double_value = k;
+
+        auto response = call_set_parameter(param);
+        if (response == NULL) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to update parameter k");
+            return;
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Successfully updated parameter k to %.2f", k);
+        }
+        for(auto result:response->results) {
+            if (!result.successful) {
+                RCLCPP_ERROR(this->get_logger(), "Failed to update parameter k: %s", result.reason.c_str());
+            } else { 
+                RCLCPP_INFO(this->get_logger(), "Parameter k updated successfully");
+            }
+        }
+    }
+
 private:
     rclcpp::Client<Patrol>::SharedPtr patrol_client;
     rclcpp::TimerBase::SharedPtr timer_;
@@ -45,6 +96,7 @@ int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<PatrolClientNode>();
+    node->update_server_param_k(4.0); // 更新服务器参数k的值为1.5
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
